47e8154cc76094be3d17726e6fa956086581f439
vasu <pentapativasu3333@gmail.com>
2023-06-18 21:57:10 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+        RWOnly obj=new RWOnly();
+
+        obj.setName("vasu");
+        obj.getName();
+    }
+
 }
